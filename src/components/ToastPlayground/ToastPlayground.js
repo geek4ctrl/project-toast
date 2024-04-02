5c7971ca747964429fe0efccf0bb927fb0a62481
Laurent Mbuyu <Laurent.Mbuyu@eoh.com>
2024-04-02 16:07:54 +0200

    [New]: Toast playground states and input properties have been fixed.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -3,13 +3,15 @@ import React, { useState } from 'react';
 import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
+import Toast from '../Toast/Toast';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
 
   const [message, setMessage] = useState('');
-  const [variant, setVariant] = useState('')
+  const [variant, setVariant] = useState(VARIANT_OPTIONS[0]);
+  const [isToastVisible, setIsToastVisible] = useState(false);
 
   return (
     <div className={styles.wrapper}>
@@ -17,6 +19,12 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      { isToastVisible ? 
+      <Toast variant={variant} handleDismiss={setIsToastVisible}>
+        {message}
+      </Toast> : 
+      <div></div> }
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
@@ -62,7 +70,7 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button>Pop Toast!</Button>
+            <Button onClick={() => { setIsToastVisible(true) }}>Pop Toast!</Button>
           </div>
         </div>
       </div>
