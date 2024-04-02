62075920e9c43d8b0485500297fe55490a07fcb7
Laurent Mbuyu <Laurent.Mbuyu@eoh.com>
2024-04-02 14:48:30 +0200

    [New]: Toast playground states and input properties have been fixed.

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '../Button';
 
@@ -7,6 +7,10 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+
+  const [message, setMessage] = useState('');
+  const [variant, setVariant] = useState('')
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +28,10 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" 
+            className={styles.messageInput}             
+            value={message}
+            onChange={(event) => {setMessage(event.target.value)}}/>
           </div>
         </div>
 
@@ -33,17 +40,20 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+            {VARIANT_OPTIONS.map((option) => {
+              return (
+                <label htmlFor={`variant-${option}`} key={`variant-${option}`}>
+                  <input
+                  id={`variant-${option}`}
+                  type='radio'
+                  name="variant"
+                  value={option}
+                  checked={option === variant}
+                  onChange={(event) => {setVariant(event.target.value)}}/>
+                  {option}
+                </label>
+              )
+            })}
           </div>
         </div>
 
